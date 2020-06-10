8b9025500de9bd80cd72ff2b1105b3fb2e6c7b4a
Konstantine <konstantinev@protonmail.com>
2020-06-10 18:13:51 -0500

    working wireframe for the program, test data

--- src/cycling.cr
+++ src/cycling.cr
@@ -1,6 +1,41 @@
 require "option_parser"
+require "csv"
 
 module Cycling
-  VERSION = "0.1.0"
-  puts "Cycling command-line interface..."
+  VERSION = "0.1.1"
+
+  # Notes
+  # Currently just works as a test version, it does the very very basic functions.
+  # TODO: Break out into components
+
+  # Set the data file
+  data = "data.csv"
+
+  # Create a new file if once doesn't exist
+  def self.create_file(data)
+    File.touch(data) # Create new file
+
+    # Create Header Row
+    first_row = CSV.build do |csv|
+      csv.row "Distance (km)", "Time (secs)", "Average Speed (km/h)", "Maximum Speed (km/h)", "Average Heart Rate", "Cadence", "Comments"
+    end
+
+    File.write(data, first_row) # Write to file
+  end
+
+  # Check if data file exist or create it
+  File.exists?(data) ? true : Cycling.create_file(data)
+
+  # TODO: Have OptionParser create flags for use in the application right around here.
+
+  # Create new row of test data
+  result = CSV.build do |csv|
+    csv.row 30, 3600, 30, 35, 130, nil, nil
+  end
+
+  # Write data to file
+  File.write(data, result, mode: "a")
+
+  # Some Text to know it all worked.
+  puts "Success: Data written to #{data}."
 end
