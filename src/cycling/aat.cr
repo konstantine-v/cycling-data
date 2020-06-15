c7e602d8c368bff4801ab15d3fabaa07c9fbac1e
Konstantine <konstantinev@protonmail.com>
2020-06-14 22:03:19 -0500

    Prepare the data for importing into CSV file

--- src/cycling/aat.cr
+++ src/cycling/aat.cr
@@ -1,22 +1,25 @@
 require "xml"
 
 class Cycling::AAT
-  def self.import(file)
-    f = File.read(file)
-    d = XML.parse(f).children
+  def self.import(file_name)
+    file = File.read(file_name)
+    a = XML.parse(file).children
 
-    # Get Time
-    d.each do |q|
-      puts q.children[1].inner_text
-    end
-
-    # Get Entries
-    d.each{|q| q.children[3].children.each{|f| f.children.each do |g|
-                                             puts g # Get attributes for lat and lon
-                                             g.children.each{|h| puts h.children} # Printing for testing purposes
-                                             # TODO: Format the data to nested array or some other type to access
-                                           end}}
+    # Prepare Entries
+    aat_arr = Array(Array(String)).new
+    a.each { |s| s.children[3].children.each { |d| d.children.each do |f|
+      ping = Array(String).new
+      next if f.inner_text.chomp("\t").chomp.empty?
+      f["lon"]?.nil? ? nil : ping << (f["lon"]?.to_s)
+      f["lat"]?.nil? ? nil : ping << (f["lat"]?.to_s)
+      f.children.each do |g|
+        g.children.to_s.empty? ? nil : ping << g.children.to_s
+      end
+      ping.empty? ? nil : aat_arr << ping
+    end } }
+    puts aat_arr
   end
+
   def self.error
     puts "There was an error paring the file, please make sure your AAT gpx file is correct..."
     exit
