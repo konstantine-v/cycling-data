c5cc9f53a207415f59db4d51aaa918213714cda1
Konstantine <konstantinev@protonmail.com>
2020-06-15 00:26:16 -0500

    0.3,3 getting the data to export to the csv - minimal

--- src/cycling.cr
+++ src/cycling.cr
@@ -2,7 +2,7 @@ require "csv"
 require "./cycling/**"
 
 module Cycling
-  VERSION = "0.3.2"
+  VERSION = "0.3.3"
 
   # Initializing
   Cycling::Options
@@ -28,44 +28,47 @@ module Cycling
   # Check if data file exist or create it
   File.exists?(ENV["file"]) ? true : Cycling.create_file
 
+  puts "Enter your ride details..."
+  puts "Fields are optional, just press ENTER to skip"
+  print "Enter Date (YYYYMMDD):"
+  d1 = read_line
+  print "Enter Distance (Miles or Kilometers):"
+  d2 = read_line
+  print "Enter Time (Enter as Seconds):"
+  d3 = read_line
+  # d4 will be set below in the conversions
+  print "Enter Max Speed (MPH or KPH):"
+  d5 = read_line
+  print "Enter Average Heart Rate:"
+  d6 = read_line
+  print "Enter Average Cadence (RPMs):"
+  d7 = read_line
+  print "Enter Comments:"
+  d8 = read_line
+  # Convert to nil to save on data size
+  d1 = d1.empty? ? nil : d1.chomp.to_i
+  d2 = d2.empty? ? nil : d2.chomp.to_f32
+  d3 = d3.empty? ? nil : d3.chomp.to_f32
+  d4 = d3.nil? || d2.nil? ? nil : (d3*60**2)/d2
+  d5 = d5.empty? ? nil : d5.chomp.to_f32
+  d6 = d6.empty? ? nil : d6.chomp.to_i16
+  d7 = d7.empty? ? nil : d7.chomp.to_f32
+  d8 = d8.empty? ? nil : d8.chomp
+
   # Create new row of test data
-  def self.build_file
+  def self.build_file(d1, d2, d3, d4, d5, d6, d7, d8)
     # Prompt user for info
-    puts "Enter your ride details..."
-    puts "Fields are optional, just press ENTER to skip"
-    print "Enter Date (YYYYMMDD):"
-    d1 = read_line
-    print "Enter Distance (Miles or Kilometers):"
-    d2 = read_line
-    print "Enter Time (Enter as Seconds):"
-    d3 = read_line
-    # d4 will be set below in the conversions
-    print "Enter Max Speed (MPH or KPH):"
-    d5 = read_line
-    print "Enter Average Heart Rate:"
-    d6 = read_line
-    print "Enter Average Cadence (RPMs):"
-    d7 = read_line
-    print "Enter Comments:"
-    d8 = read_line
-    # Convert to nil to save on data size
-    d1 = d1.empty? ? nil : d1.chomp.to_i
-    d2 = d2.empty? ? nil : d2.chomp.to_f32
-    d3 = d3.empty? ? nil : d3.chomp.to_f32
-    d4 = d3.nil? || d2.nil? ? nil : (d3*60**2)/d2
-    d5 = d5.empty? ? nil : d5.chomp.to_f32
-    d6 = d6.empty? ? nil : d6.chomp.to_i16
-    d7 = d7.empty? ? nil : d7.chomp.to_f32
-    d8 = d8.empty? ? nil : d8.chomp
     # Build CSV Row
     result = CSV.build do |csv|
       csv.row d1, d2, d3, d4, d5, d6, d7, d8
     end
+
     # Write data to file
     File.write(ENV["file"], result, mode: "a")
+
+    # Some Text to know it all worked.
+    puts "Success: Data written to #{ENV["file"]}."
   end
 
-  Cycling.build_file
-  # Some Text to know it all worked.
-  puts "Success: Data written to #{ENV["file"]}."
+  Cycling.build_file(d1, d2, d3, d4, d5, d6, d7, d8)
 end
--- src/cycling/aat.cr
+++ src/cycling/aat.cr
@@ -1,23 +1,58 @@
 require "xml"
 
 class Cycling::AAT
+
+  # Get distance of coordinates in kilometers
+  def self.distance(loc1, loc2)
+    rad_per_deg = Math::PI/180  # PI / 180
+    rm = 6371000                  # Earth radius in kilometers
+
+    dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+    rm * c # Delta in meters
+  end
+
   def self.import(file_name)
     file = File.read(file_name)
     a = XML.parse(file).children
 
     # Prepare Entries
-    aat_arr = Array(Array(String)).new
+    aat_arr = Array(Array(String|Float64)).new
     a.each { |s| s.children[3].children.each { |d| d.children.each do |f|
-      ping = Array(String).new
+      ping = Array(String|Float64).new
       next if f.inner_text.chomp("\t").chomp.empty?
-      f["lon"]?.nil? ? nil : ping << (f["lon"]?.to_s)
-      f["lat"]?.nil? ? nil : ping << (f["lat"]?.to_s)
+      f["lon"]?.nil? ? nil : ping << (f["lon"]?.to_s.to_f64)
+      f["lat"]?.nil? ? nil : ping << (f["lat"]?.to_s.to_f64)
       f.children.each do |g|
         g.children.to_s.empty? ? nil : ping << g.children.to_s
       end
       ping.empty? ? nil : aat_arr << ping
     end } }
-    puts aat_arr
+
+    dist_arr = Array(Float64|Int64).new
+    (0..(aat_arr.size-1)).each do |m|
+      # (m = 5) ? exit : nil
+      if m != 5
+        dist_arr << Cycling::AAT.distance([aat_arr[m][0].to_f64, aat_arr[m][1].to_f64],[aat_arr[m+1][0].to_f64, aat_arr[m+1][1].to_f64])
+      else
+      end
+    end
+
+    fin_date = aat_arr[0][-1].to_s.gsub(/T.*/im,"").gsub('-',"")
+    fin_dist = (dist_arr.sum*1000).to_i
+    # TODO: Calculate avg speed
+    # puts (aat_arr[0][-1].to_s.gsub(/.*T/im,"").gsub(".000Z","").to_i)
+    # ( aat_arr[-1][-1].to_s.gsub(/T.*/im,"").gsub('-',""))
+    ENV["file"] ||= "./data.csv"
+    File.exists?(ENV["file"]) ? true : Cycling.create_file
+    Cycling.build_file(fin_date, fin_dist, nil, nil, nil, nil, nil, nil)
   end
 
   def self.error
